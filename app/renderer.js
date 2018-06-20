ec74cec6e5693e10394e44b5c901da47fcd422f5
DummybugStudios <siddhartha.mish2000@gmail.com>
2018-06-20 18:17:39 +0100

    Removed youtube-audio-stream dependency

--- app/renderer.js
+++ app/renderer.js
@@ -1,13 +1,15 @@
-
-var youtubeStream = require("youtube-audio-stream");
-var lame = require('lame');
-var Speaker = require('speaker');
-var decoder = lame.Decoder
+const ytdl    = require('ytdl-core')
+const Speaker = require('speaker')
+const lame    = require('lame')
+const decoder  = lame.Decoder
+const speaker = new Speaker()
 
 const url = "https://www.youtube.com/watch?v=bZ7jUGCVFss" 
 
 function playMusic(){
-   youtubeStream(url).pipe(decoder()).pipe(new Speaker())
+    ytdl(url,{
+        filter:'audioonly'
+    }).pipe(decoder()).pipe(speaker)
 }
 
 module.exports={playMusic}
